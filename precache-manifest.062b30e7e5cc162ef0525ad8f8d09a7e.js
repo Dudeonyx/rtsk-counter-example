675294893ac669aaae974c9a42bec865b548eadb
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-06 15:22:31 +0100

    Updates

---
+++ precache-manifest.062b30e7e5cc162ef0525ad8f8d09a7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb1edaa98976a678f7a0",
+    "url": "/rtsk-counter-example/static/js/runtime~main.fb1edaa9.js"
+  },
+  {
+    "revision": "99e486a1e689bcd7c3fb",
+    "url": "/rtsk-counter-example/static/js/main.99e486a1.chunk.js"
+  },
+  {
+    "revision": "66d88d575209c7c8a697",
+    "url": "/rtsk-counter-example/static/js/1.66d88d57.chunk.js"
+  },
+  {
+    "revision": "64d0089256433f5a260fb9bcca8a2701",
+    "url": "/rtsk-counter-example/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rtsk-counter-example/precache-manifest.062b30e7e5cc162ef0525ad8f8d09a7e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rtsk-counter-example/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
